c03c546acbe92bbfafbfda1d2f0eac599402ebd2
mohd <generalassemblyse@gmail.com>
2025-08-06 16:14:57 +0300

    add to Route

--- react-jwt-auth-template/src/App.jsx
+++ react-jwt-auth-template/src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
         <Route path='/' element={<Landing />}/>
         }
         <Route path="/sign-up" element={<SignUp handleSignUp={handleSignUp} user={user} />}/>
-        {/* <Route path='*' element={<h1>404 Error</h1>}/> */}
+        <Route path='*' element={<h1>404 Error</h1>}/>
 
         <Route path="/sign-in" element={<SignIn handleSignIn={handleSignIn} user={user}/>}/>
       </Routes>
